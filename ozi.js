5468e561b175a174bfd0c37f89a586481a01b2f6
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-01-09 17:04:53 +0300

    Sekme Koruması Eklenmistir Star Atmayı Unutma

--- ozi.js
+++ ozi.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const request = require('request');
 const ChannelData = require('./models/Channel.js');
 const RoleData = require('./models/role.js');
+const userRoles = require('./models/UserRoles.js');
 
 mongoose.connect(ayarlar.MongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.on("open", async() => {
@@ -251,4 +252,30 @@ client.on("channelDelete", channel => {
   client.channels.cache.get(ayarlar.backupkanal).send("Silinen Kanal İsim: `" + channel.name + "`");
 });
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+client.on("presenceUpdate", async (eski, yeni) => {
+  const ozicik = Object.keys(yeni.user.presence.clientStatus);
+  const embed = new Discord.MessageEmbed();
+  let kanal = client.channels.cache.get(ayarlar.backupkanal)
+  const roller = yeni.member.roles.cache.filter((e) => e.editable && e.name !== "@everyone" && [8, 4, 2, 16, 32, 268435456, 536870912, 134217728, 128].some((a) => e.permissions.has(a)));
+  if (!yeni.user.bot && yeni.guild.id === ayarlar.guildID && [8, 4, 2, 16, 32, 268435456, 536870912, 134217728, 128].some((e) => yeni.member.permissions.has(e)) ) {
+    const sunucu = client.guilds.cache.get(ayarlar.guildID);
+    if (sunucu.ownerID === yeni.user.id) return;
+    if (ozicik.find(e => e === "web")) {
+      await userRoles.findOneAndUpdate({ guildID: ayarlar.guildID, userID: yeni.user.id }, { $set: { roles: roller.map((e) => e.id) } }, { upsert: true });
+      await yeni.member.roles.remove(roller.map((e) => e.id), "Tarayıcıdan Giriş Yapıldığı İçin Rolleri Alındı.");
+      if (kanal) kanal.send(embed.setDescription(`${yeni.user.toString()} tarayıcıdan giriş yaptığı için yetkileri alındı! \n\n**Rollerin Listesi:** \n${roller.map((e) => `<@&${e.id}>`).join("\n")}`).setAuthor(yeni.member.displayName, yeni.user.avatarURL({ dynamic: true })).setFooter(ayarlar.BotFooter, client.guilds.cache.get(ayarlar.guildID).iconURL({ dynamic: true })).setTimestamp().setColor(yeni.member.displayHexColor));
+    } 
+  }
+  if (!ozicik.find(e => e === "web")) {
+    const veri = await userRoles.findOne({ guildID: ayarlar.guildID, userID: yeni.user.id });
+    if (!veri) return;
+    if (veri.roles || veri.roles.length) {
+      await veri.roles.map(e => yeni.member.roles.add(e, "Tarayıcıdan Çıkış Yapıldığı İçin Rolleri Geri Verildi.").then(async () => {
+        await userRoles.findOneAndDelete({ guildID: ayarlar.guildID, userID: yeni.user.id });
+        if (kanal) kanal.send(embed.setDescription(`${yeni.user.toString()} tarayıcıdan çıkış yaptığı için yetkileri verildi! \n\n**Rollerin Listesi:** \n${veri.roles.map((e) => `<@&${e}>`).join("\n")}`).setAuthor(yeni.member.displayName, yeni.user.avatarURL({ dynamic: true })).setFooter(ayarlar.BotFooter, client.guilds.cache.get(ayarlar.guildID).iconURL({ dynamic: true })).setTimestamp().setColor(yeni.member.displayHexColor));
+      }).catch(() => {}));
+    }
+  }
+});
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
