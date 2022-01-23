7dc2589491ef68ddd6e29a34bb47cfb6e5fe72ad
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-01-23 16:19:48 +0300

    Star Atmayı Unutmayınız

--- models/Güvenli.js
+++ models/Güvenli.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Güvenli = new mongoose.Schema({
     guildID: { type: String, default: "" },
-    Safe: Array,
+    Safe: {type: Array, default: ""}, 
 });
 
 module.exports = mongoose.model("Güvenli", Güvenli);
